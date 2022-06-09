f170a68653f544e028ed62ed92d0c5d95fe2a06e
cg-yeong <ds1506027@gmail.com>
2022-06-09 15:56:39 +0900

    add then

---
+++ LandScapeScrollCollectionSection/LandScapeScrollCollectionSection/VC+SnapKit.swift
@@ -0,0 +1,17 @@
+//
+//  VC+SnapKit.swift
+//  LandScapeScrollCollectionSection
+//
+//  Created by root0 on 2022/06/09.
+//
+
+import Foundation
+import SnapKit
+
+extension ViewController {
+    
+    func addSnapView() {
+        
+    }
+    
+}
