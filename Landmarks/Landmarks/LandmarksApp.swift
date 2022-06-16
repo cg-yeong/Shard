b79cd7d6341384a1b5ce1431fd368fc5ff5e2265
cg-yeong <ds1506027@gmail.com>
2022-06-16 18:19:22 +0900

    swiftUI tutorial - 1

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by root0 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
